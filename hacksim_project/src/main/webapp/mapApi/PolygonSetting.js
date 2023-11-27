0da9b0e0464ab776a9b82731116a4b7cf7b734c9
SMHRD <SMHRD@218.149.140.106>
2023-11-27 17:12:23 +0900

    색깔 수정

--- hacksim_project/src/main/webapp/mapApi/PolygonSetting.js
+++ hacksim_project/src/main/webapp/mapApi/PolygonSetting.js
@@ -2,250 +2,250 @@
 //폴리곤에 대한 세부설정
 var polygon_01 = sop.polygon(polygon_01, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.2
 });
 
 var polygon_02 = sop.polygon(polygon_02, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.2
 });
 
 var polygon_03 = sop.polygon(polygon_03, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.2
 });
 
 var polygon_04 = sop.polygon(polygon_04, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.2
 });
 
 var polygon_05 = sop.polygon(polygon_05, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.4
 });
 
 var polygon_06 = sop.polygon(polygon_06, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.4
 });
 
 var polygon_07 = sop.polygon(polygon_07, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.4
 });
 
 var polygon_08 = sop.polygon(polygon_08, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.4
 });
 
 var polygon_09 = sop.polygon(polygon_09, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.4
 });
 
 var polygon_10 = sop.polygon(polygon_10, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.4
 });
 
 var polygon_11 = sop.polygon(polygon_11, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.4
 });
 
 var polygon_12 = sop.polygon(polygon_12, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.4
 });
 
 var polygon_13 = sop.polygon(polygon_13, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.4
 });
 
 var polygon_14 = sop.polygon(polygon_14, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.4
 });
 
 var polygon_15 = sop.polygon(polygon_15, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.6
 });
 
 var polygon_16 = sop.polygon(polygon_16, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.6
 });
 
 var polygon_17 = sop.polygon(polygon_17, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.6
 });
 
 var polygon_18 = sop.polygon(polygon_18, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.6
 });
 
 var polygon_19 = sop.polygon(polygon_19, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.6
 });
 
 var polygon_20 = sop.polygon(polygon_20, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.6
 });
 
 var polygon_21 = sop.polygon(polygon_21, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.8
 });
 
 var polygon_22 = sop.polygon(polygon_22, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.8
 });
 
 var polygon_23 = sop.polygon(polygon_23, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.8
 });
 
 var polygon_24 = sop.polygon(polygon_24, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.8
 });
 
 var polygon_25 = sop.polygon(polygon_25, {
 	stroke: true,
-	color: "#191970",
+	color: "#FAAC58",
 	weight: 3,
 	opacity: 1,
 	fill: true,
-	fillColor: "#191970",
+	fillColor: "#FAAC58",
 	fillOpacity: 0.95
 });
